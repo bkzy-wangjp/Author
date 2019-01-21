2354adf1d7467a8e12bbe81f62a3f51cccfae462
bkzy-wangjp <120726080@qq.com>
2019-01-21 15:06:23 +0800

    更新校验输出

--- MicETL-Author/MicETLAuthor.go
+++ MicETL-Author/MicETLAuthor.go
@@ -18,8 +18,7 @@ func main() {
 	fmt.Println("MicETL软件授权码生成器")
 	fmt.Println("版本:V1.0")
 	fmt.Println("发布日期:2019年1月21日")
-	thismcode := Author.MachineCodeEncrypt() //本机机器码
-	fmt.Println("本机机器码是:", thismcode)
+
 	ips := 0
 	ipc := 0
 	pserr := 3
@@ -39,14 +38,17 @@ iptpswd:
 		goto iptpswd
 	}
 
-	fmt.Println("请依次输入授权数量、用户名和机器码,中间以空格分割:")
+	thismcode := Author.MachineCodeEncrypt() //本机机器码
+	fmt.Println("本机机器码:", thismcode)
+	fmt.Println("------------开始授权码生成工作---------------")
+	fmt.Println("请依次输入授权数量、用户名和机器码,中间以空格分割,注意大小写敏感:")
 inputloop:
 	i, err := fmt.Scanf("%d %s %s", &cnt, &username, &mcode)
 	if i < 3 || err != nil {
 		ipc++
 		if ipc > 1 {
 			fmt.Println("输入错误:", err.Error())
-			fmt.Println("请依次输入授权数量、用户名和机器码,中间以空格分割:")
+			fmt.Println("请依次输入授权数量、用户名和机器码,中间以空格分割,注意大小写敏感:")
 			ipc = 0
 		}
 		goto inputloop
@@ -60,15 +62,19 @@ inputloop:
 	authCode := Author.AuthorizationCodeEncrypt(cnt, username, mcode)
 	fmt.Println("授权码为:", authCode)
 	c, u, mc := Author.AuthorizationCodeDecrypt(key, authCode)
-	fmt.Println("授权解码校验:", c, u, mc)
-	/*	ok := SaveAuthMsg(cnt, mcode, key, mac, username, authCode) //存储信息
-		if ok {
-			fmt.Println("授权信息保存成功!")
-		} else {
-			fmt.Println("授权信息保存失败!")
-		}*/
+	fmt.Println("授权解码校验:")
+	fmt.Println("授权数量:", c)
+	fmt.Println("授权用户:", u)
+	fmt.Println("主板型号:", key)
+	fmt.Println("网卡MAC:", mc)
+	ok := SaveAuthMsg(cnt, mcode, key, mac, username, authCode) //存储信息
+	if ok {
+		fmt.Println("授权信息保存成功!")
+	} else {
+		fmt.Println("授权信息保存失败!")
+	}
 	fmt.Println("------------分割线,请继续生成授权码,或者直接关闭----------------")
-	fmt.Println("请依次输入授权数量、用户名和机器码,中间以空格分割:")
+	fmt.Println("请依次输入授权数量、用户名和机器码,中间以空格分割,注意大小写敏感:")
 	ipc = 0
 	goto inputloop
 }
@@ -100,7 +106,7 @@ func SaveAuthMsg(cnt int, mcode, key, mac, username, authCode string) bool {
 		time.Now().Format("2006-01-02 15:04:05"),
 		mcode, authCode, key, mac, cnt, username)
 	//每次循环用的都是tx内部的连接，没有新建连接，效率高
-	fmt.Println("SQL:", sqlInsert)
+	//fmt.Println("SQL:", sqlInsert)
 	_, err = tx.Exec(sqlInsert)
 	if err != nil {
 		fmt.Println("数据存储失败", err.Error)
